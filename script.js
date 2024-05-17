530436f2f4028d49cb3ed847a0d814b7798f5b07
Guilherme SouzaDias <Sou.guilhermedias@gmail.com>
2024-05-17 00:08:27 +0000

    ajuste geral, adicionando as pastas

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ verificarTema(body, botaoTema)
 
 const botoesAssunto = document.querySelectorAll(".assuntos button")
 botoesAssunto.forEach(botao => {
-    botao.addEventListener("click", selecionaAssunto)
+    botao.addEventListener("click", selecionarAssunto)
 })
 
 function selecionarAssunto(evento) {
-    const classBotao = evento.target.className
+    const classeBotao = evento.target.className
     const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
