cb6cbbd86e64576963e64bb2bb9f2e54f05881c1
Guilherme SouzaDias <Sou.guilhermedias@gmail.com>
2024-05-21 16:49:22 +0000

    refazendo o quiz.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ botoesAssunto.forEach(botao => {
     botao.addEventListener("click", selecionarAssunto)
 })
 
-function selecionarAssunto(evento) {
+function selecionarAssunto(evento) { //parte correta
     const classeBotao = evento.target.className
     const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
-    window.location.href = "./pages/quiz/quiz.html"
+    window.location.href = "./pages/quiz/quiz.html" //linha correta
 }
